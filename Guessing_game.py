4e5fee574bd17b798dc31ceb9c4155cd5c1d5555
Mateo <mateohu2002@gmail.com>
2025-03-22 19:08:56 +0100

    Updated with better options

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,19 +1,32 @@
 import random
 
 def guessing_game():
+    try:
+        maximum=int(input('Please tell me the max value. \n'))
+        minimum=int(input('Please tell me the min value. \n'))
+        if maximum<=minimum:
+            print('Invalid range. Min must be less than max')
+            return
 
-    maximo=int(input('Please tell me the max value. \n'))
-    minimo=int(input('Please tell me the min value. \n'))
-
-    numero=random.randint(minimo,maximo)
-    print(numero)
-    x=int(input('Now, try to guess my number! \n'))
-    print('Wow! You guessed it at your first try!') if x==numero else None
-    while x != numero:
-        try: 
-            x=int(input('Try again! \n'))
-            if x==numero:
-                print('Congratulations! You guessed it!')
-        except ValueError:
-            print("That's not an integer! \n")
+        numero=random.randint(minimum,maximum)
+        tries=0
+        print(numero)
+        
+        x=int(input('Now, try to guess my number! \n'))
+        
+        while x != numero:
+            try: 
+                tries += 1
+                if x < numero:
+                    x=int(input('Too low! Try again. \n'))
+                elif x > numero:
+                    x=int(input('Too high! Try again. \n'))
+            except ValueError:
+                print("That's not an integer! \n")
+        if tries == 0:
+            print('Wow! You guessed it at your first try!')
+        else:
+            print(f'Congratulations, you guessed it at {tries} tries!')
+    except ValueError:
+        print('Invalid input of range. Please enter integers')
 guessing_game()
