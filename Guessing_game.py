6aff14a3a6a885de7bf11cc80331d28b62606920
Mateo <mateohu2002@gmail.com>
2025-03-22 18:35:52 +0100

    My first try

--- Guessing_game.py
+++ Guessing_game.py
@@ -1,16 +1,19 @@
 import random
 
-maximo=int(input('Please tell me the max value. \n'))
-minimo=int(input('Please tell me the min value. \n'))
+def guessing_game():
 
-numero=random.randint(minimo,maximo)
-print(numero)
-x=int(input('Now, try to guess my number! \n'))
-print('Wow! You guessed it at your first try!') if x==numero else None
-while x != numero:
-    try: 
-        x=int(input('Try again! \n'))
-        if x==numero:
-            print('Congratulations! You guessed it!')
-    except ValueError:
-        print("That's not an integer! \n")
+    maximo=int(input('Please tell me the max value. \n'))
+    minimo=int(input('Please tell me the min value. \n'))
+
+    numero=random.randint(minimo,maximo)
+    print(numero)
+    x=int(input('Now, try to guess my number! \n'))
+    print('Wow! You guessed it at your first try!') if x==numero else None
+    while x != numero:
+        try: 
+            x=int(input('Try again! \n'))
+            if x==numero:
+                print('Congratulations! You guessed it!')
+        except ValueError:
+            print("That's not an integer! \n")
+guessing_game()
